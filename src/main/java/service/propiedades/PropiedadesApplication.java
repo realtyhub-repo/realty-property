d02961b07ad7cdf00a913ce4db3a17d5c87ddf80
yerson-zip <yersonochoa.2005@gmail.com>
2026-09-19 14:08:44 -0500

    primer commit de este repo

---
+++ src/main/java/service/propiedades/PropiedadesApplication.java
@@ -0,0 +1,13 @@
+package service.propiedades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropiedadesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropiedadesApplication.class, args);
+    }
+
+}
